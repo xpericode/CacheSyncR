9fd0801d63ec2a15a3ced721259ecdee0e24d16c
Marcel Wijnands <marcel@shynet.nl>
2015-02-13 00:26:58 +0100

    Added projects.

---
+++ CacheSyncR.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XperiCode.CacheSyncR.Client")]
+[assembly: AssemblyDescription("Keep your local cache in sync with a remote data source using SignalR.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XperiCode")]
+[assembly: AssemblyProduct("CacheSyncR.Client")]
+[assembly: AssemblyCopyright("Copyright © Marcel Wijnands 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5e555fdf-cbf3-4ba4-b075-4c2e008d6e18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-preview")]
